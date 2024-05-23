1f69c0d4ef2140c50e1d0e56999725d3060d67a5
豪 邓 <2393162177@qq.com>
2024-05-23 19:42:38 +0800

    httpx引入

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import module.Logger.logger as log
-log.init()
-log.info("Starting")
-for i in range(0,1000):
-    log.debug(f"{i}")
-log.info("end")
+import httpx
+import asyncio
+
+async def request(url):
+    async with httpx.AsyncClient() as client:
+        client.follow_redirects = True
+        r = await client.get(url)
+        log.warn(url)
+        log.info(r.text)
+    
+if __name__ == '__main__':
+    log.init()
+    log.info("begin")
+    asyncio.run(request("http://www.qidian.com"))
+    log.info("end")
